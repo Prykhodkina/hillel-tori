dfb6c4f961aa4f1a74ac4903cad024ca912e62d0
Prykhodkina <in_extremis@ymail.com>
2025-06-05 11:10:09 +0300

    4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 'use strict'
+
+    // 1. Username (string)
+    let username = "Tori April";
+
+    // 2. Age (number)
+    let age = 32;
+
+    // 3. Is the user verified? (boolean)
+    let isVerified = true;
+
+    // 4. Registration date (string as date)
+    let registrationDate = new Date(2023,4,14);
+
+    // 5. User card number (BigInt)
+    let cardNumber = 1234567890123456n;
+
+    // 6. Promo code (null or string)
+    let promoCode = null;
+
+    // 7. Alternate name (not defined)
+    let altName;
+
+    // 8. Number of bonuses per account (number)
+    let bonusPoints = 150;
+
+    // Show output in console
+    console.log("Username:", username);
+    console.log("Age:", age);
+    console.log("Is Verified:", isVerified);
+    console.log("Registration Date:", registrationDate);
+    console.log("Card Number:", cardNumber);
+    console.log("Promo Code:", promoCode);
+    console.log("Alternate Name:", altName);
+    console.log("Bonus Points:", bonusPoints);
+
