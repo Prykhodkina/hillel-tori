aceedde908e2dd85e880be79451c39990deb902e
Prykhodkina <in_extremis@ymail.com>
2025-06-06 13:14:53 +0300

    3

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let number1 = Number(prompt("Enter number"));
+let number2 = Number(prompt("Enter number"));
+let number3 = Number(prompt("Enter number"));
+
+let mean = (number1 + number2 + number3) / 3;
+
+alert("Average: " + mean);
