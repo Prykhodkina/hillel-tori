54a1c602d92001a7a3eb8b072b85174c999b958a
Prykhodkina <in_extremis@ymail.com>
2025-07-08 02:04:06 +0300

    first

--- js/script.js
+++ js/script.js
@@ -1,114 +1,132 @@
 `use strict`
 
-function calculateNumAverage (arr) {
+const num = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+
+function calculateAverage(arr) {
     let sum = 0;
-    let total = 0;
+    let count = 0;
+     for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (typeof item === `number` && !isNaN(item)) {
+            sum += item;
+            count++;
+        }
+    }
+    if (count === 0) {
+        return `No numeric values found`;
+    }
+    return sum / count;
+}
+const average = calculateAverage(num);
+console.log(`${average}`);
+
+function findMaxAndIndex(arr) {
+    if (!arr || arr.length === 0) {
+        return {max: undefined, index: undefined};
+    }
+    const max = Math.max(...num);
+    const index = num.indexOf(max);
+    return {max: max, index: index};
+}
+const maxAndIndex = findMaxAndIndex(num);
+console.log(`Max element: ${Math.max(...num) }`);
+console.log(`Index of maximum number: ${maxAndIndex.index}`);
+
+
+function minElement(arr) {
+    if (!arr === arr.length) {
+        return `No numeric values found`;
+    }
+    let min = Math.min(...num);
+    let index = 0;
     for (let i = 0; i < arr.length; i++) {
-        if
-        (typeof arr[i] === 'number' && !isNaN(arr[i])) {
-            sum += arr[i];
-            total ++;
-        }
-
-    }
-    if (total === 0){
-        return `No numeric value found`;
-    }
-return sum / total;
-}
-
-const mixArr = [52, `a`, `b`, NaN, true, null, 1, 2, 37];
-const result = calculateNumAverage(mixArr);
-console.log(result);
-
-
-function doMath(x, znak, y) {
-    x = Number(x);
-    y = Number(y);
-
-    if (isNaN(x) || isNaN(y)) {
-        console.log(`Error: x and y must be valid number`);
-        return;
-    }
-
-    let resultF;
-    switch (znak) {
-        case `+`:
-            resultF = x + y;
-            break;
-        case `-`:
-            resultF = x - y;
-            break;
-        case `*`:
-            resultF = x * y;
-            break;
-        case `/`:
-            if (y === 0) {
-                console.log(`Error: Division by zero`);
-                return;
-            }
-            resultF = x / y;
-            break;
-        case `%`:
-            resultF = x % y;
-            break;
-        case `^`:
-            resultF = x ** y;
-            break;
-        default:
-            console.log(`Error: Unsupported operator`);
-            return;
-    }
-
-    console.log(`Result: ${resultF}`);
-}
-const x = prompt(`Enter a number`);
-const znak = prompt(`Enter the operation sign (+, -, *, /, %, ^):`);
-const y = prompt(`Enter the second number:`);
-
-doMath(x, znak, y);
-
-function fill2DArr() {
-    const mainLength = parseInt(prompt(`Enter number of rows:`));
-    if (isNaN(mainLength) || mainLength <= 0) {
-        console.log(`Error: Invalid number of rows.`);
-        return;
-    }
-const rows = [];
-for (let i = 0; i < mainLength; i++) {
-    const innerLength = parseInt(prompt(`Enter number of elements in row ${i + 1}:`));
-    if (isNaN(innerLength) || innerLength <= 0) {
-        console.log(`Error: Invalid number of elements.`);
-        return;
-    }
-    const innerArr = [];
-    for (let i = 0; i < innerLength; i++) {
-        const value = prompt(`Enter number of columns:`);
-        innerArr.push(value);
-    }
-    rows.push(innerArr);
-}
-
-console.log(`result:`);
-console.log(rows);
-}
-fill2DArr();
-
-function removeSymbol(str, symbolRe) {
-    const removeOne = new Set(symbolRe);
-    let upshot = ``;
-
-    for (let symbol of str) {
-        if (!removeOne.has(symbol)) {
-            upshot += symbol;
+        if (arr[i] === min) {
+            min = arr[i];
+            index = i;
         }
     }
-    return upshot;
+    return {min: min, index: index};
 }
-const inputString = prompt(`Enter a string:`);
-const symbolInput = prompt(`Enter symbol to remove (as one string, e.g., 'l,o')`);
+const minIndex = minElement(num);
+console.log(`Min element: ${minIndex.min}`);
+console.log(`Index of minimum number: ${minIndex.index}`);
 
-const symbolRe = symbolInput.split(``);
-const upshot = removeSymbol (inputString, symbolRe);
+function countNegative(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            count++;
+        }
+    }
+    return count;
+}
+const negative = countNegative(num);
+console.log(`Negative element: ${negative}`);
 
-console.log(`Result:`, upshot);
+
+function oddPositiveElement(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (item > 0 && item % 2 !== 0) {
+            count++;
+        }
+    }
+    return count;
+}
+const oddPositive = oddPositiveElement(num);
+console.log(`Number of odd positive elements: ${oddPositive}`);
+
+function countPositiveElement(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (item > 0 && item % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+}
+const positiveElement = countPositiveElement(num);
+console.log(`The number of even positive elements: ${positiveElement}`);
+
+function sumPositiveNum(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0 && arr[i] % 2 === 0){
+            count += arr[i];
+        }
+    }
+    return count;
+}
+const sum = sumPositiveNum(num);
+console.log(`Sum of even positive elements: ${sumPositiveNum(num)}`);
+
+function sumNegativeNum(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0 && arr[i] % 2 !== 0){
+            count += arr[i];
+        }
+    }
+    return count;
+}
+const sumNegative = sumNegativeNum(num);
+console.log(`Sum of even positive elements: ${sumNegativeNum(num)}`);
+
+const product = num.reduce((acc, item) => acc * item);
+console.log(`Product: ${product}`);
+
+function somethingElse(arr) {
+    if (!arr || arr.length === 0) {
+        return {max: undefined};
+    }
+    const max = Math.max(...arr);
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i] !== max) {
+            arr[i] = 0;
+        }
+    }
+    return arr;
+}
+console.log(somethingElse(num));
