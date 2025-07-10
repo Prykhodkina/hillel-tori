aa743348381e24272a7eaff3d875598ee72b6d8f
Prykhodkina <in_extremis@ymail.com>
2025-07-10 22:54:46 +0300

    first

--- js/script.js
+++ js/script.js
@@ -1,37 +1,21 @@
 `use strict`
 
-function findValuesByKey(obj, targetKey) {
-const result = [];
-function search(curr) {
-    if (typeof curr !== `object` || curr === null) return;
-    for (let key in curr) {
-        if (key === targetKey) {
-            result.push(curr[key]);
-        }
-        if (typeof curr[key] === `object` && curr[key] !== null) {
-            search(curr[key]);
-        }
-    }
 
-}
-search(obj);
-return result;
-}
-const data = {
-    id: 1,
-    name: "root",
-    meta: {
-        id: 2,
-        parent: {
-            id: 3,
-            name: "leaf",
-        },
-    },
-    array: [
-        { id: 4 },
-        { name: "node", children: [{ id: 5 }] },
-    ],
-};
-const result = findValuesByKey(data, `id`);
-console.log(result);
 
+
+const select = document.getElementById("filter");
+const cards = document.querySelectorAll(".card");
+
+select.addEventListener("change", function () {
+    const selectedCategory = select.value;
+
+    cards.forEach(card => {
+        const cardCategory = card.getAttribute(`data-category`);
+
+        if (selectedCategory === "all" || cardCategory === selectedCategory) {
+            card.classList.remove(`hidden`);
+        } else {
+            card.classList.add(`hidden`);
+        }
+    });
+});
