3d963e31f7a94d4695b618d8ef3c9b9b54f4b434
Prykhodkina <in_extremis@ymail.com>
2025-07-11 03:25:33 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 `use strict`
 
+const theme = document.querySelector(`#toggle-theme`);
+function backgroundColor(e) {
+    document.body.classList.toggle(`dark-theme`);
+    console.log(document.body.classList)
+}
+theme.addEventListener(`click`, backgroundColor);
 
-
-
-const select = document.getElementById("filter");
-const cards = document.querySelectorAll(".card");
-
-select.addEventListener("change", function () {
-    const selectedCategory = select.value;
-
-    cards.forEach(card => {
-        const cardCategory = card.getAttribute(`data-category`);
-
-        if (selectedCategory === "all" || cardCategory === selectedCategory) {
-            card.classList.remove(`hidden`);
-        } else {
-            card.classList.add(`hidden`);
-        }
-    });
-});
