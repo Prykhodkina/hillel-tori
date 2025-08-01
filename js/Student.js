4fb0ac748b0932c39edc6b4662617c1b30bd96e6
Prykhodkina <in_extremis@ymail.com>
2025-08-02 02:58:16 +0300

    1

--- js/Student.js
+++ js/Student.js
@@ -1,56 +1,60 @@
 'use strict';
 
-function Student(firstName, lastName, birthYear, grades) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthYear = birthYear;
-    this.grades = grades;
-    this.attendance = new Array(25);
-    this.attendanceIndex = 0;
+class Student {
+    constructor(firstName, lastName, birthYear, grades) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.grades = grades;
+        this.attendance = new Array(25);
+        this.attendanceIndex = 0;
+    }
+
+    getAge() {
+        return new Date().getFullYear() - this.birthYear;
+    }
+
+    getAverageGrade() {
+        if (this.grades.length === 0) return 0;
+        const total = this.grades.reduce((sum, g) => sum + g, 0);
+        return total / this.grades.length;
+    }
+
+    present() {
+        if (this.attendanceIndex < 25) {
+            this.attendance[this.attendanceIndex++] = true;
+        } else {
+            console.warn("Attendance is full. No more entries allowed.");
+        }
+    }
+
+    absent() {
+        if (this.attendanceIndex < 25) {
+            this.attendance[this.attendanceIndex++] = false;
+        } else {
+            console.warn("Attendance is full. No more entries allowed.");
+        }
+    };
+
+    summary() {
+        const avgGrade = this.getAverageGrade();
+
+        const recorded = this.attendance.filter(val => val !== undefined);
+        const total = recorded.length;
+        const present = recorded.filter(val => val === true).length;
+
+        const attendanceRatio = total > 0 ? present / total : 0;
+
+        if (avgGrade > 90 && attendanceRatio > 0.9) {
+            return "Молодець!";
+        } else if (avgGrade > 90 || attendanceRatio > 0.9) {
+            return "Добре, але можна краще";
+        } else {
+            return "Редиска!";
+        }
+    };
 }
-Student.prototype.getAge = function () {
-    return new Date().getFullYear() - this.birthYear;
-};
 
-Student.prototype.getAverageGrade = function () {
-    if (this.grades.length === 0) return 0;
-    const total = this.grades.reduce((sum, g) => sum + g, 0);
-    return total / this.grades.length;
-};
-
-Student.prototype.present = function () {
-    if (this.attendanceIndex < 25) {
-        this.attendance[this.attendanceIndex++] = true;
-    } else {
-        console.warn("Attendance is full. No more entries allowed.");
-    }
-};
-
-Student.prototype.absent = function () {
-    if (this.attendanceIndex < 25) {
-        this.attendance[this.attendanceIndex++] = false;
-    } else {
-        console.warn("Attendance is full. No more entries allowed.");
-    }
-};
-
-Student.prototype.summary = function () {
-    const avgGrade = this.getAverageGrade();
-
-    const recorded = this.attendance.filter(val => val !== undefined);
-    const total = recorded.length;
-    const present = recorded.filter(val => val === true).length;
-
-    const attendanceRatio = total > 0 ? present / total : 0;
-
-    if (avgGrade > 90 && attendanceRatio > 0.9) {
-        return "Молодець!";
-    } else if (avgGrade > 90 || attendanceRatio > 0.9) {
-        return "Добре, але можна краще";
-    } else {
-        return "Редиска!";
-    }
-};
 
 const student1 = new Student("Anna", "Shevchenko", 2004, [100, 95, 92]);
 const student2 = new Student("Bohdan", "Melnyk", 2003, [88, 84, 79]);
