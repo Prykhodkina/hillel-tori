bf8945c8d92127feec0af5509998dcccdc7f8d06
Prykhodkina <in_extremis@ymail.com>
2025-08-12 14:06:01 +0300

    2

--- js/HistoryTracker.js
+++ js/HistoryTracker.js
@@ -25,11 +25,9 @@ const tracker = new HistoryTracker();
 document.getElementById("page1").addEventListener("click", () => {
     tracker.push("page1");
 });
-
 document.getElementById("page2").addEventListener("click", () => {
     tracker.push("page2");
 });
-
 document.getElementById("back").addEventListener("click", () => {
     tracker.back();
 });
